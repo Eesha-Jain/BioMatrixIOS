04bb72dcb4482d6c2cb23821a2ef8f9f2a9f8f1f
Eesha-Jain <queenoftherace@gmail.com>
2021-05-20 19:52:03 -0700

    Initial Commit

---
+++ BioMatrix/BioMatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioMatrixApp.swift
+//  BioMatrix
+//
+//  Created by Jain, Shikha on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct BioMatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BioMatrix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BioMatrix
+//
+//  Created by Jain, Shikha on 5/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
