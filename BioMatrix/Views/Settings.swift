6eabc06ff62afe143c380599ef3860b5ad84bb16
Eesha-Jain <queenoftherace@gmail.com>
2021-06-24 21:46:04 -0700

    Fixed file organization

---
+++ BioMatrix/Views/Settings.swift
@@ -0,0 +1,15 @@
+//File Settings.swift in Project BioMatrix: Created by Eesha Jain on 6/24/21
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
